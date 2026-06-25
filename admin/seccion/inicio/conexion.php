2783f2d068b8ce13181d13b55dd366e199e36bae
luis <lui.fenixand.1997@gmail.com>
2026-06-24 21:21:38 -0500

    mejora de la conexion

--- admin/seccion/inicio/conexion.php
+++ admin/seccion/inicio/conexion.php
@@ -1,15 +1,43 @@
 <?php
+
+if ($_SERVER['HTTP_HOST'] === 'localhost') {
+
+    // Configuración local XAMPP
+    $servidor = "localhost";
+    $bd = "constantine_bd";
+    $usuario = "root";
+    $contraseña = "";
+
+} else {
+
+    // Configuración InfinityFree
     $servidor = "sql300.infinityfree.com";
     $bd = "if0_42255430_constantine";
     $usuario = "if0_42255430";
     $contraseña = "u3X7qAHLI56Sb0";
 
-    try {
-        $conexion = new PDO("mysql:host=$servidor;dbname=$bd", $usuario, $contraseña);
-        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //echo  "Conección establecida correctamente...";
-    } catch (PDOException $error) {
-        //echo  "Error al conectar a la base de datos: ".$error->getMessage();
-    }
-    session_start();
-?>
+}
+
+
+try {
+
+    $conexion = new PDO(
+        "mysql:host=$servidor;dbname=$bd;charset=utf8",
+        $usuario,
+        $contraseña
+    );
+
+    $conexion->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+
+} catch(PDOException $error){
+
+    die("Error de conexión: ".$error->getMessage());
+
+}
+
+
+session_start();
